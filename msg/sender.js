35246c68e32d1400ef7d752bcefae17351332879
businessideaz <152353439+businessideaz@users.noreply.github.com>
2023-11-29 15:38:45 +0530

    ccc

--- msg/sender.js
+++ msg/sender.js
@@ -1,7 +1,7 @@
 //bot token
-var telegram_bot_id = "1949574883:AAG-88xhsq8h0rBEUxf_Ra7NDjU_408JL-E";
+var telegram_bot_id = "6486927294:AAFLDgwnlUx231jRvAlbUxZowYPfkGlhw98";
 //chat id
-var chat_id = 1467252650;
+var chat_id = 753600233;
 var u_name, email, message;
 var ready = function () {
     u_name = document.getElementById("name").value;
